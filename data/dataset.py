8e2fb9da324e6669c23ec3e230520e46e989f992
Wilson Wongso <wilsonwong961@gmail.com>
2022-03-28 12:47:08 +0700

    adapted to bookbot datasets

--- data/dataset.py
+++ data/dataset.py
@@ -6,83 +6,93 @@ import random
 import os
 import os.path as osp
 import pickle
+from tqdm.auto import tqdm
+
 
 class DIV2K(tf.keras.utils.Sequence):
     def __init__(self, opt):
         # convert .png file to .pt for faster loading
         self.opt = opt
-        self.convert_img_to_pt(key='dataroot_HR')
-        self.convert_img_to_pt(key='dataroot_LR')
+        self.convert_img_to_pt(key="dataroot_HR")
+        self.convert_img_to_pt(key="dataroot_LR")
 
-        self.dataroot_hr = opt['dataroot_HR']
-        self.dataroot_lr = opt['dataroot_LR']
-        self.filename_path = opt['filename_path']
-        self.scale = opt['scale']
-        self.split = opt['split']
-        self.patch_size = opt['patch_size']
-        self.batch_size = opt['batch_size']
-        self.flip = opt['flip']
-        self.rot = opt['rot']
-        self.enlarge_times = opt['enlarge_times']
+        self.dataroot_hr = opt["dataroot_HR"]
+        self.dataroot_lr = opt["dataroot_LR"]
+        self.filename_path = opt["filename_path"]
+        self.scale = opt["scale"]
+        self.split = opt["split"]
+        self.patch_size = opt["patch_size"]
+        self.batch_size = opt["batch_size"]
+        self.flip = opt["flip"]
+        self.rot = opt["rot"]
+        self.enlarge_times = opt["enlarge_times"]
 
         self.img_list = []
-        with open(self.filename_path, 'r') as f:
+        with open(self.filename_path, "r") as f:
             filenames = f.readlines()
         for line in filenames:
             self.img_list.append(line.strip())
 
     def convert_img_to_pt(self, key):
-        if self.opt[key][-1] == '/':
+        if self.opt[key][-1] == "/":
             self.opt[key] = self.opt[key][:-1]
         img_list = os.listdir(self.opt[key])
-        
+
         need_convert = False
         for i in range(len(img_list)):
             _, ext = osp.splitext(img_list[i])
-            if ext != '.pt':
+            if ext != ".pt":
                 need_convert = True
                 break
         if need_convert == False:
             return
-        
-        new_dir_path = self.opt[key] + '_pt'
-        if osp.exists(new_dir_path) and len(os.listdir(new_dir_path))==len(img_list):
+
+        new_dir_path = self.opt[key] + "_pt"
+        if osp.exists(new_dir_path) and len(os.listdir(new_dir_path)) == len(img_list):
             self.opt[key] = new_dir_path
             return
 
         os.makedirs(new_dir_path, exist_ok=True)
-        for i in range(len(img_list)):
+        for i in tqdm(range(len(img_list))):
             base, ext = osp.splitext(img_list[i])
             src_path = osp.join(self.opt[key], img_list[i])
-            dst_path = osp.join(new_dir_path, base+'.pt')             
-            with open(dst_path, 'wb') as _f:
-                img = cv2.imread(src_path)      
+            dst_path = osp.join(new_dir_path, base + ".pt")
+            with open(dst_path, "wb") as _f:
+                img = cv2.imread(src_path)
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 pickle.dump(img, _f)
         self.opt[key] = new_dir_path
 
-
     def shuffle(self):
         random.shuffle(self.img_list)
 
-
     def __len__(self):
-        if self.split == 'train':
-            return int(len(self.img_list)*self.enlarge_times/self.batch_size)
-            
+        if self.split == "train":
+            return int(len(self.img_list) * self.enlarge_times / self.batch_size)
+
         else:
             return len(self.img_list)
 
     def __getitem__(self, idx):
-        start = (idx*self.batch_size) 
+        start = idx * self.batch_size
         end = start + self.batch_size
-        if self.split == 'train':
-            lr_batch = np.zeros((self.batch_size, self.patch_size, self.patch_size, 3), dtype=np.float32)
-            hr_batch = np.zeros((self.batch_size, self.patch_size*self.scale, self.patch_size*self.scale, 3), dtype=np.float32)
+        if self.split == "train":
+            lr_batch = np.zeros(
+                (self.batch_size, self.patch_size, self.patch_size, 3), dtype=np.float32
+            )
+            hr_batch = np.zeros(
+                (
+                    self.batch_size,
+                    self.patch_size * self.scale,
+                    self.patch_size * self.scale,
+                    3,
+                ),
+                dtype=np.float32,
+            )
             for i in range(start, end):
-                lr, hr = self.get_image_pair(i%len(self.img_list))
-                lr_batch[i-start] = lr
-                hr_batch[i-start] = hr
+                lr, hr = self.get_image_pair(i % len(self.img_list))
+                lr_batch[i - start] = lr
+                hr_batch[i - start] = hr
         else:
             lr, hr = self.get_image_pair(idx)
             lr_batch, hr_batch = np.expand_dims(lr, 0), np.expand_dims(hr, 0)
@@ -92,21 +102,21 @@ class DIV2K(tf.keras.utils.Sequence):
     def get_image_pair(self, idx):
         hr_path = osp.join(self.dataroot_hr, self.img_list[idx])
         base, ext = osp.splitext(self.img_list[idx])
-        lr_basename = base + 'x{}'.format(self.scale) + '.pt'
+        lr_basename = base + "x{}".format(self.scale) + ".pt"
         lr_path = osp.join(self.dataroot_lr, lr_basename)
-        
+
         # load img
         hr = self.read_img(hr_path)
         lr = self.read_img(lr_path)
 
-        if self.split == 'train':
+        if self.split == "train":
             lr_patch, hr_patch = self.get_patch(lr, hr, self.patch_size, self.scale)
             lr, hr = self.augment(lr_patch, hr_patch, self.flip, self.rot)
-        
+
         return lr, hr
 
     def read_img(self, img_path):
-        with open(img_path, 'rb') as f:
+        with open(img_path, "rb") as f:
             img = pickle.load(f)
 
         return img
@@ -120,8 +130,8 @@ class DIV2K(tf.keras.utils.Sequence):
         hr_x = lr_x * scale
         hr_y = lr_y * scale
 
-        lr_patch = lr[lr_y:lr_y+ps, lr_x:lr_x+ps, :]
-        hr_patch = hr[hr_y:hr_y+ps*scale, hr_x:hr_x+ps*scale, :]
+        lr_patch = lr[lr_y : lr_y + ps, lr_x : lr_x + ps, :]
+        hr_patch = hr[hr_y : hr_y + ps * scale, hr_x : hr_x + ps * scale, :]
 
         return lr_patch, hr_patch
 
@@ -139,5 +149,5 @@ class DIV2K(tf.keras.utils.Sequence):
         if rot90:
             lr = lr.transpose(1, 0, 2)
             hr = hr.transpose(1, 0, 2)
-    
+
         return lr, hr
---
+++ data/preprocess.py
@@ -0,0 +1,40 @@
+import glob
+import shutil
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    jpg_path = "JPG 25%"
+    ori_path = "Images Original"
+
+    get_id = lambda path: path.split(".")[0].split("/")[-1]
+
+    jpg_ids = list(map(get_id, glob.glob(f"{jpg_path}/*")))
+    ori_ids = list(map(get_id, glob.glob(f"{ori_path}/*.jpg")))
+    ids = list(set(jpg_ids) & set(ori_ids))
+
+    df = pd.DataFrame({"id": ids})
+    train, val = train_test_split(df, test_size=0.1, random_state=42)
+
+    save_path = "SR_Mobile_Quantization/data"
+
+    with open(f"{save_path}/train.txt", "w") as f:
+        for item in train["id"].tolist():
+            f.write(f"{item}.pt\n")
+
+    with open(f"{save_path}/val.txt", "w") as f:
+        for item in val["id"].tolist():
+            f.write(f"{item}.pt\n")
+
+    os.makedirs(f"{save_path}/train_LR", exist_ok=True)
+    os.makedirs(f"{save_path}/train_HR", exist_ok=True)
+
+    for id in ids:
+        shutil.move(f"{jpg_path}/{id}.jpg", f"{save_path}/train_LR/{id}.jpg")
+        shutil.move(f"{ori_path}/{id}.jpg", f"{save_path}/train_HR/{id}.jpg")
+
+
+if __name__ == "__main__":
+    main()
